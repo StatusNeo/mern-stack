4680be6890d20e4e6ec9dc0cbdbef8f9a1df7cd1
NishkarshRaj <500060720@stu.upes.ac.in>
2020-07-23 18:59:22 +0530

    Static Website Completely Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }

dc46ec69014a0919fbd84ecf5eeb05dffc8781cf
NishkarshRaj <500060720@stu.upes.ac.in>
2020-07-23 18:00:52 +0530

    Setup Client Interface with React Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <AppNavbar />
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  }
+  
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  
+  render() {
+    return (
+<div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <Container>
+	  <NavbarBrand href="/">Shopping List</NavbarBrand>
+	  <NavbarToggler onClick={this.toggle} />
+   	  <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+	      <NavItem>
+	        <NavLink href="https://www.github.com/NishkarshRaj">GitHub</NavLink>
+	      </NavItem>
+	    </Nav>
+	  </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+    );    
+  }
+}
+
+export default AppNavbar;
+

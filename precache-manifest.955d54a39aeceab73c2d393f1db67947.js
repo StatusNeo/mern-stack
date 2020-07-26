b1ba26f552bd3abc8c479d3a86a074440d577df6
NishkarshRaj <500060720@stu.upes.ac.in>
2020-07-26 08:14:56 +0530

    Updates

---
+++ precache-manifest.955d54a39aeceab73c2d393f1db67947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c886532ab6d2189844621f813a40ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a5b0a9ecc90fbaf223",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a683b8456ff169ab5353",
+    "url": "/static/css/main.c5275767.chunk.css"
+  },
+  {
+    "revision": "42a5b0a9ecc90fbaf223",
+    "url": "/static/js/2.bfeb244d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.bfeb244d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a683b8456ff169ab5353",
+    "url": "/static/js/main.9a7b5f0a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
